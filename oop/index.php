0289064f4c645afba41eaab0751fc99649826190
Avdolll <01.abdulaziz.2023@gmail.com>
2025-02-07 15:54:30 +0700

    Revisi

--- oop/index.php
+++ oop/index.php
@@ -1,9 +1,21 @@
 <?php
 require_once "animal.php";
+require_once "Frog.php"; 
+require_once "Ape.php"; 
 
 $sheep = new Animal("shaun");
 
 echo $sheep->name . "<br>";
 echo $sheep->legs . "<br>";
-echo $sheep->cold_blooded . "<br>";
+echo $sheep->cold_blooded . "<br>"; 
+
+$sungokong = new Ape("kera sakti");
+echo $sungokong->name . "<br>"; 
+echo $sungokong->legs . "<br>";
+$sungokong->yell(); 
+
+$kodok = new Frog("gamabunta");
+echo $kodok->name . "<br>"; 
+echo $kodok->legs . "<br>"; 
+$kodok->jump();
 ?>
